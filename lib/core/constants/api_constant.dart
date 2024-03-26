ed891ca3857770ca40f16d573a75c16ce1eb483c
Kashish Saxena <kashish.saxena@successive.tech>
2024-03-26 17:20:25 +0530

    file replace

--- lib/core/constants/api_constant.dart
+++
@@ -1,3 +0,0 @@
-class ApiConstants{
-  static const String apiKey = 'AIzaSyD0pG4tOIE94ul5fLmC9OF0RvaRC-xF7Mw';
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
-import 'package:gemini_demo/core/constants/api_constant.dart';
+import 'package:gemini_demo/core/constants/api_constants.dart';
 import 'package:gemini_demo/core/di/locator.dart';
 import 'package:gemini_demo/ui/views/my_app.dart';
 
